75f8116490464666e56f544e30561a2a677b0355
redstrait <minbum9999@gmail.com>
2025-05-02 10:34:15 +0900

    feat: 에셋 & 메인 맵 & 캐릭터 추가
    - Externals 폴더에 필요 에셋 추가
    - Main Scene에 임시 맵 제작
    - 캐릭터 추가 & 이동 구현

---
+++ Assets/Scripts/BaseController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BaseController : MonoBehaviour
+{
+    protected Rigidbody2D _rigidbody;
+
+    [SerializeField] private SpriteRenderer characterRenderer;
+
+    protected Vector2 movementDirection = Vector2.zero; // 이동 방향
+    public Vector2 MovementDirection { get { return movementDirection; } }
+
+    protected Vector2 lookDirection = Vector2.zero; // 시선 방향
+    public Vector2 LookDirection { get { return lookDirection; } }
+
+    protected virtual void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        HandleAction();
+        Rotate(lookDirection);
+    }
+
+    protected virtual void FixedUpdate()
+    {
+        Movment(movementDirection);
+    }
+
+    protected virtual void HandleAction()
+    {
+
+    }
+
+    private void Movment(Vector2 direction)
+    {
+        direction = direction * 5; // 이동 속도
+
+        _rigidbody.velocity = direction;
+    }
+
+    private void Rotate(Vector2 direction) // 시선 회전
+    {
+        float rotZ = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        bool isLeft = Mathf.Abs(rotZ) > 90f; // 90도보다 클 때 왼쪽을 바라봄
+
+        characterRenderer.flipX = isLeft; // 캐릭터 이미지를 왼족으로 회전
+    }
+}
